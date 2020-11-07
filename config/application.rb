e52f7165a78b03c84a57aabdf563f7627a401f0a
EC2 Default User <ec2-user@ip-172-31-47-64.ec2.internal>
2020-11-07 15:56:44 +0000

    first

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module CL3
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+     config.load_defaults 5.2
+
+   config.generators do |g|
+     g.assets false          # CSS, JavaScriptファイルは生成しない
+     g.test_framework false  # testファイルは生成しない
+   end
+  end
+end
