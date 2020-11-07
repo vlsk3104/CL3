563c5a948983dfbf2768022f5522632590ffc4c7
EC2 Default User <ec2-user@ip-172-31-47-64.ec2.internal>
2020-11-07 16:34:24 +0000

    修正

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'bootstrap-sass'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -47,7 +48,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 group :test do
-  
+  gem 'capybara', '>= 2.15'
   gem 'webdrivers'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -6,21 +6,18 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CL3
+module CooklogForNote
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-     config.load_defaults 5.2
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
 
-   config.generators do |g|
-     g.assets false          # CSS, JavaScriptファイルは生成しない
-     g.test_framework false  # testファイルは生成しない
-     g.test_framework :rspec,
+    config.generators do |g|
+      g.assets false
+      g.test_framework :rspec,
         controller_specs: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
-   end
+    end
   end
 end
